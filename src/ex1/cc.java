5fb0d638fe509c63e066922bdf1c6947968d3ffc
Viraj Bhalala <virajbhalala@gmail.com>
2014-09-13 22:22:06 -0400

    Update cc.java

--- src/ex1/cc.java
+++ src/ex1/cc.java
@@ -5,55 +5,101 @@
 package ex1;
 import java.util.Scanner;
 
+
 public class cc {
+
+private static final String[][] String = null;
+
 	public static void main(String[] args) {
-		System.out.println ("Enter Encryption: ");
-		Scanner scanner =new Scanner (System.in);
-		String s = scanner.nextLine();	
+		
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		int stringSize = input.length();
+		char[] storage = new char [stringSize];
+		
+		
+		
+		//get each char in original string (using for loop) and replace it with char with 3 increments (exculde punctuation) xyz->abc
+		for (int x= 0; x<stringSize; x++)
+		{
 			
-		System.out.println (translator(s));
+			char character = input.charAt(x);
+			
+			if(character == 'X'){
+				character = 'A';
+				storage [x] = character;
+			}
+			
+			else
+					if(character == 'Y'){
+				character = 'B';
+				storage [x] = character;
+			}
+			else 
+				if(character == 'Z'){
+				character = 'C';
+				storage [x] = character;
+			}
+			else
+					if(character == 'x'){
+				character = 'a';
+				storage [x] = character;
+			}
+			else
+					if(character == 'y'){
+				character = 'b';
+				storage [x] = character;
+			}
+			else
+					if(character == 'z'){
+				character = 'c';
+				storage [x] = character;
+			}
+			else 
+				if (character >='a'&&character <='w' ){
+				character++;
+				character++;
+				character++;
+				storage [x] = character;
+			}
+			else
+				if (character >='A'&&character <='W' ){
+				character++;
+				character++;
+				character++;
+				storage [x] = character;
+			}
+			
+				
+			
+			else
+					if (character >='['&&character <='`' ){
+				
+				storage [x] = character;
+			}
+			else	if (character >='{'&&character <='~' ){
+				
+				storage [x] = character;
+			}
+			else if (character >='!'&&character <='@' ){
+				
+				storage [x] = character;
+			}
+			
+			else  if (character ==' ' ){
+				
+				storage [x] = character;
+			}
+			
+			
+			}
+			
+	
+		
+		String modifiedString = new String(storage);
+		System.out.println(modifiedString);
+		
+		
+	}
 
-	}
-	public static String translator(String s){
-		String temp = "";
-		for (int i =0; i<s.length(); i++){
-			if(s.charAt(i)==' '){
-				temp += ' ';
-			}
-			else if(s.charAt(i)=='!'){
-				temp += '!';
-			}
-			else if(s.charAt(i)=='x'){
-				temp += 'a';
-			}
-			else if(s.charAt(i)=='X'){
-				temp += 'A';
-			}
-				
-			else if(s.charAt(i)=='Y'){
-				temp += 'B';
-			}
-			
-			else if(s.charAt(i)=='y'){
-				temp += 'b';
-			}
-			else if(s.charAt(i)=='Z'){
-				temp += 'C';
-			}
-			
-			else if(s.charAt(i)=='z'){
-				temp += 'c';
-			}
-			//do same thing for y andz
-			else{
-				temp += (char)(s.charAt(i) + 3);
-				}
-				
-			}
-		return temp;
-	}
-	
-	
-	
 }
-
